5121233aa03136fd791744d32cfb5634e57fe319
Ben3Coder <56853870+benjitran2009@users.noreply.github.com>
2024-12-19 12:09:26 -0600

    Update Banana

---
+++ src/assets/main.js
@@ -0,0 +1,11 @@
+/*******************************************************
+ *
+ * This code uses assets from Banana Clicker
+ * 
+ * Copyright (C) Ben3Coder Software
+ * (https://ben3coder.dev)
+ * (https://github.com/benjitran2009/banana-clicker/)
+ * 
+ *******************************************************/
+
+// insert code...
